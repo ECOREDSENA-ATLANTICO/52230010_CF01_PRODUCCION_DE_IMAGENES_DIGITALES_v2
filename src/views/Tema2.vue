a092e6f15f65ee8e7e5f03b6c201de0514cba833
emilydev <flizcanor@icloud.com>
2024-09-24 21:14:27 -0500

    se actualiza tema 2 y 3

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -1,13 +1,161 @@
 <template lang="pug">
-.curso-main-container.pb-3
-  BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
-        span 2
-      h1 Titulo de primer nivel
-
-</template>
+  .curso-main-container.pb-3
+    BannerInterno
+    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+      .titulo-principal.color-acento-contenido
+        .titulo-principal__numero
+          span 2
+        h1 
+          i briefing
+      
+      .row.mb-5
+        .col-md-7
+          p David Roca (2007) expresa que el trabajo de la agencia empieza con la entrega del 
+            i briefing 
+            | por parte del cliente y contiene la siguiente información básica: 
+          ul.red_checks
+            li Las características de la empresa.
+            li El beneficio o diferencia significativa del producto o servicio.
+            li Público objetivo a quien va dirigido la comunicación. 
+            li 
+              b El mercado: 
+              | información de ventas, precio, segmentación.
+            li Objetivos de comunicación. 
+            li El presupuesto para invertir en la campaña.
+            li Medios que utilizar. 
+          p Si se necesita más información del cliente se realiza un contrabriefing y se convoca a una reunión con el cliente de la cual surge el 
+            i briefing 
+            | definitivo para creación. Para profundizar en esta temática consulte:
+        
+        .col-md-5
+          figure
+            img(src='@/assets/curso/temas/tema2/img01.png', alt='Imagen Decorativa')
+      
+      .row
+        .mx-auto.col-xs-12.col-md-10
+          .tarjeta-avatar-b.mb-5
+            .tarjeta-avatar-b__img
+              img.img-t.m-auto(src='@/assets/curso/temas/tema1/img21.svg', alt='Imagen Decorativa')
+            .tarjeta.color-primario.row.align-items-center.bg03
+              .col-md-9.mb-3.mb-sm-0
+                p.p-2.d-block.d-lg-none
+                p.mb-0.text-white
+                  b Consejos para escribir un 
+                    i brief 
+                    | creativo. 
+                p.mb-0.text-white Disponible en el siguiente enlace:
+              .col-md-2.mb-3.mb-sm-0.p-2
+                a.boton.color-acento-botones(href="https://www.youtube.com/watch?v=6vlN4DZVQGc" target="_blank")
+                  span Enlace
+                  i.fas.fa-link
+  
+      figure.d-flex.align-items-center.mb-4
+        img.img-t.d-inline-block.align-middle.me-2(src='@/assets/curso/temas/tema1/img08.png', alt='Imagen Decorativa')
+        h2.mb-0 Principios en el diseño de un logo
+  
+      .row.mb-5
+        .col-md-5
+          figure
+            img(src='@/assets/curso/temas/tema2/img05.png', alt='Imagen Decorativa')
+      
+        .col-md-7
+          p Al recibir el 
+            i brief 
+            | del cliente se establece la estrategia creativa. Cada agencia de publicidad tiene una metodología de trabajo distinta. Algunas agencias adoptan el 
+            i copy strategy 
+            | de la multinacional 
+            i Procter & Gamble, 
+            | la UPS 
+            i (Unique Selling Proposition )
+            |, el 
+            i briefing 
+            | creativo como la agencia Ogilvy & Mather o la 
+            i Star Strategy 
+            | de la agencia RSCG. Por otro lado, Pricken (2004) comparte una sucesión de técnicas de creatividad denominada el Catálogo 
+            i Kickstart 
+            | para lograr trasmitir el concepto publicitario. 
+          p En este caso, se profundizará en el 
+            i briefing 
+            | creativo para lograr consolidar el mensaje publicitario. A partir del 
+            i brief 
+            | entregado u obtenido del anunciante, inicia el 
+            i briefing 
+            | creativo analizando la información escrita y oral que aportó el anunciante o cliente; esas acciones son desarrolladas por un equipo responsable de la estrategia de comunicación conformado por un director creativo y el 
+            i planner, 
+            | entre otros entre ellos generan el flujo de acciones se pueden consultar en la siguiente figura:
+  
+      .row.mb-5
+        .col-md-10.mx-auto
+          .titulo_sexto.mb-4(data-aos="fade-right")
+              h5.mb-0 Figura 2. 
+                span Briefing Creativo
+          figure
+            img(src='@/assets/curso/temas/tema2/img03.svg', alt='Imagen Decorativa')
+            figcaption Nota, SENA (2014)
+          
+      .row.mb-5
+        .col-md-5
+          figure
+            img(src='@/assets/curso/temas/tema2/img04.png', alt='Imagen Decorativa')
+        .col-md-7
+          .cajon.color-primario.p-4
+            ul.bullets
+              li 
+                b Datos de identificación de la organización: 
+                | cliente, fecha, campaña y marca.
+              li 
+                b Objetivo de la comunicación: 
+                | qué se quiere conseguir con la campaña.
+              li 
+                b Público objetivo: 
+                | grupo al que se dirige la campaña de acuerdo a su perfil demográfico, socio-económico y cultural.
+              li 
+                b Posicionamiento: 
+                | lugar que ocupa en la mente del público objetivo. 
+              li 
+                b Promesa básica: 
+                | lo que ofrece el producto. 
+              li 
+                b 
+                  i Reason Why: 
+                | argumentación o evidencia que sostiene el beneficio prometido.
+              li  
+                b UPV: 
+                | La ventaja diferenciadora del producto o servicio.
+              li 
+                b Tono de comunicación: 
+                | Enfoque que se debe utilizar para expresar el posicionamiento: racional o emocional.
+              li 
+                b Eje de la Campaña: 
+                | efecto psicológico que se desea sobre la audiencia analizando sus motivaciones y necesidades.  
+              li 
+                b Concepto de campaña: 
+                | define la línea creativa de cómo se van a expresar los beneficios o ventajas del producto o servicio. 
+              li 
+                b Medios: 
+                | formatos publicitarios que desea utilizar para transmitir la idea creativa. 
+  
+      p Para profundizar en esta temática consulte el ejemplo:
+  
+      .row
+        .mx-auto.col-xs-12.col-md-10
+          .tarjeta-avatar-b.mb-5
+            .tarjeta-avatar-b__img
+              img.img-t.m-auto(src='@/assets/curso/temas/tema1/img21.svg', alt='Imagen Decorativa')
+            .tarjeta.color-primario.row.align-items-center.bg03
+              .col-md-9.mb-3.mb-sm-0
+                p.p-2.d-block.d-lg-none
+                p.mb-0.text-white 
+                  b Muestra de 
+                    i Brief 
+                    | Creativo: 
+                p.mb-0.text-white  Disponible en el siguiente enlace:
+              .col-md-2.mb-3.mb-sm-0.p-2
+                a.boton.color-acento-botones(href="http://es.scribd.com/doc/68381546/Muestra-de-Brief-Creativo-Pepsi-Co#force_seo" target="_blank")
+                  span Enlace
+                  i.fas.fa-link
+       
+  </template>
 
 <script>
 export default {
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -1,13 +1,160 @@
 <template lang="pug">
-.curso-main-container.pb-3
-  BannerInterno
-  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
-      .titulo-principal__numero
-        span 3
-      h1 Titulo de primer nivel
-
-</template>
+  .curso-main-container.pb-3
+    BannerInterno
+    .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+      .titulo-principal.color-acento-contenido
+        .titulo-principal__numero
+          span 3
+        h1 Idea creativa
+  
+      .row.mb-5
+        .col-md-4
+          figure
+            img.mb-4(src='@/assets/curso/temas/tema3/img01.png', alt='Imagen Decorativa')
+        .col-md-8
+          p El siguiente paso se basa en una estrategia de contenido, en la cual se analiza la estrategia creativa, que en este caso es el 
+            i briefing 
+            | creativo. Este documento es clave, ya que recoge toda la información necesaria para abordar el problema de comunicación y definir el enfoque correcto. A partir del 
+            i briefing, se selecciona el eje de comunicación y se elabora el concepto o la idea creativa, que se proyecta en el mensaje publicitario.
+          p Una vez establecido el eje de comunicación, se elabora el concepto o la idea creativa, que es la representación central del mensaje que se desea transmitir. Esta idea se proyecta en el mensaje publicitario de manera que conecte eficazmente con el público objetivo, alineándose con los valores y objetivos de la marca, tal como se analiza en la figura.
+  
+      .row.mb-5
+        .col-md-10.mx-auto
+          .titulo_sexto.mb-4(data-aos="fade-right")
+              h5.mb-0 Figura 3. 
+                span Estrategia de contenido.
+          figure
+            img(src='@/assets/curso/temas/tema3/img02.svg', alt='Imagen Decorativa')
+            figcaption Nota. Sena (2014)
+      
+      figure.d-flex.align-items-center.mb-4
+        img.img-t.d-inline-block.align-middle.me-2(src='@/assets/curso/temas/tema1/img08.png', alt='Imagen Decorativa')
+        h2.mb-0 Estrategia de Contenido
+  
+      .mb-5
+        p La estrategia de contenido estructura el mensaje publicitario. En la estrategia se desarrollan los siguientes pasos: Primero se selecciona el eje de comunicación. El beneficio argumentado en el 
+          i briefing 
+          | creativo es la ventaja o valor que se destaca como el principal atractivo del producto o servicio, y que se utilizará como eje central en la campaña publicitaria. El creativo debe apelar a la satisfacción del individuo por medio de sus necesidades, deseos, esperanzas, emociones o ambiciones.  
+        p Ahora el creativo debe representar esa satisfacción en un concepto o idea creativa que estructure el contenido del mensaje publicitario. Por ejemplo, se puede evocar directamente la satisfacción o de manera inducida. Un ejemplo de evocación directa es: “Lanita no sube el precio” o “Pasta Blanca, nueva textura, mejor disolución”. 
+  
+      .bloque-texto-g.color-secundario.p-3.p-sm-4.p-md-5.mb-5
+        .bloque-texto-g__img(
+          :style="{'background-image': `url(${require('@/assets/curso/temas/tema3/img03.jpg')})`}"
+        )
+        .bloque-texto-g__texto.p-4
+          p.mb-0 La evocación inducida exige que el público objetivo interprete el significado de la idea creativa; por ejemplo: Agua Escondida, todavía quedan océanos por descubrir o explorar, ¿quién ha dicho que la belleza no se puede comprar? La elaboración del concepto o idea es la parte más compleja del proceso creativo en la publicidad.
+  
+      figure.d-flex.align-items-center.mb-5
+        img.img-t.d-inline-block.align-middle.me-2(src='@/assets/curso/temas/tema1/img08.png', alt='Imagen Decorativa')
+        h2.mb-0 Estrategia de codificación del mensaje publicitario 
+      p En este punto, ha llegado el momento de expresar el concepto o idea creativa en textos, sonidos, colores y símbolos. Para lograrlo, es necesario comprender la audiencia y qué tipo de iconos, frases e imágenes pueden generar significado, efectos emotivos y conductuales en ésta. En esta etapa del proceso se le da la forma al mensaje al generar un anuncio dependiendo del medio y soporte en el cual vaya a ser difundido. 
+      p Por ejemplo, si el proyecto será destinado a un medio impreso, se realizan bocetos o maquetas; si se destina a un medio auditivo, se realiza un guión o si se trata de un medio audiovisual, se realiza un 
+        i storyboard 
+        | o guión gráfico.
+      p.mb-5 Esta codificación se desarrolla en un anuncio donde radica la fuerza visual y el 
+        i copy 
+        | o frase publicitaria. Son responsables directos de la codificación, el director de arte quien traduce la idea creativa en imágenes o audio y el 
+        i copy 
+        | quien redacta los textos que aparecen en el anuncio publicitario.
+  
+      figure.d-flex.align-items-center.mb-4
+        img.img-t.d-inline-block.align-middle.me-2(src='@/assets/curso/temas/tema1/img08.png', alt='Imagen Decorativa')
+        h2.mb-0 Representación de la idea creativa
+  
+      .row.mb-5
+        .col-md-4
+          figure
+            img.mb-4(src='@/assets/curso/temas/tema3/img04.png', alt='Imagen Decorativa')
+        .col-md-8
+          p Un anuncio efectivo transmite la idea creativa de manera directa o inducida, mediante códigos publicitarios que ayudan a comunicar el mensaje de forma impactante y comprensible. La clave está en cómo se presenta la idea para captar la atención del público y generar una respuesta deseada.
+          p Para la representación de la idea creativa en un anuncio, se pueden utilizar varias técnicas, como lluvia de ideas, mapas mentales, bocetos, 100 mph 
+            i thinking, 
+            | dibujos o ideas fuera de la zona de confort. La lluvia de ideas es una técnica fundamental que permite explorar múltiples conceptos y enfoques sin restricciones, promoviendo la creatividad y la innovación. Los mapas mentales ayudan a organizar las ideas y visualizar las conexiones entre diferentes elementos del anuncio.
+          p Otras técnicas útiles incluyen la elaboración de bocetos y dibujos. El "100 mph 
+            i thinking 
+            | " se refiere a la generación rápida de ideas sin censura, lo que puede llevar a descubrimientos sorprendentes. Finalmente, pensar creativamente anima a explorar enfoques no convencionales, enriqueciendo el proceso creativo y potenciando la efectividad del anuncio. A continuación, se presentan unos bocetos para un medio impreso desarrollado para una campaña de bebidas: 
+  
+  
+      .tarjeta.bg01.p-4.mb-5
+        .tarjeta.p-5
+          SlyderA(tipo="b")
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-md-5.mb-4.mb-md-0
+                p 
+                  strong Ideas Innovadoras 
+                p Se muestra un boceto de la técnica 100 mph 
+                  i thinking, 
+                  | que se centra en encontrar ideas innovadoras y soluciones creativas que satisfagan las necesidades y gustos de los consumidores.
+  
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema3/img06.jpg', alt='Imagen Decorativa')
+                  figcaption Nota. Rotimi Solola 2024
+            
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-md-5.mb-4.mb-md-0
+                p 
+                  strong Objetivo
+                p El boceto muestra un mensaje publicitario para la representación de una idea creativa. La propuesta conceptual, destaca cómo se proyecta la idea en el mensaje final para captar la atención del público objetivo.
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema3/img07.jpg', alt='Imagen Decorativa')
+                  figcaption Nota. Rotimi Solola 2024
+  
+            .row.justify-content-center.align-items-center.my-lg-4
+              .col-md-5.mb-4.mb-md-0
+                p 
+                  strong Marca
+                p El logo en la publicidad final debe destacar claramente la identidad de la marca. Además, debe ser visible y memorable, integrándose armónicamente con el mensaje y el diseño general para reforzar la imagen y asegurar la correcta asociación con la marca.
+  
+              .col-md-7
+                figure
+                  img(src='@/assets/curso/temas/tema3/img08.jpg', alt='Imagen Decorativa')
+                  figcaption Nota. Rotimi Solola 2024
+  
+      p.mb-5 Los pasos seguidos para la representación de la idea creativa se detallan a continuación:
+      .row.mb-5
+        .col-md-4
+          figure
+            img.img-t.mx-auto(src='@/assets/curso/temas/tema3/img05.svg', alt='Imagen Decorativa')
+        .col-md-8
+          SlyderF(columnas="col-lg-6 col-xl-6")
+            .tarjeta.tarjeta--grisclaro.p-4 
+              h2.big 01
+              p.text-center Se estudió un 
+                i briefing 
+                | creativo y mediante la aplicación de la técnica de 100 mph 
+                i thinking, 
+                | donde se generaron tantas ideas visuales como se pudo en un tiempo limitado.
+  
+            .tarjeta.tarjeta--grisclaro.p-4
+              h2.big 02
+              p.text-center Se eligió una idea y se desarrolló un boceto.
+  
+            .tarjeta.tarjeta--grisclaro.p-4
+              h2.big 03
+              p.text-center Se logró la publicidad final. 
+      
+      
+      .row
+        .mx-auto.col-xs-12.col-md-10
+          p.mb-4 Para profundizar en esta temática consulte la presentación:
+  
+          .tarjeta-avatar-b.mb-5
+            .tarjeta-avatar-b__img
+              img.img-t.m-auto(src='@/assets/curso/temas/tema1/img21.svg', alt='Imagen Decorativa')
+            .tarjeta.color-primario.row.align-items-center.bg03
+              .col-md-9.mb-3.mb-sm-0
+                p.p-2.d-block.d-lg-none
+                p.mb-0.text-white 
+                  b La Publicidad, el 
+                  i brief, 
+                  | la creatividad y el diseño gráfico publicitario. 
+              .col-md-2.mb-3.mb-sm-0.p-2
+                a.boton.color-acento-botones(href="http://es.scribd.com/doc/68381546/Muestra-de-Brief-Creativo-Pepsi-Co#force_seo" target="_blank")
+                  span Enlace
+                  i.fas.fa-link
+  </template>
 
 <script>
 export default {
